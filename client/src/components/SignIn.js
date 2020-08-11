ed15562939fdd3277927bf421ccab936a4223aca
Parikshit Singh <pk223933@gmail.com>
2020-08-11 20:02:59 +0530

    sign in component UI created

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -5,8 +5,8 @@ class SignIn extends React.Component {
 	render() {
 		return (
 			<div className="signIn-component">
-				<div className="row w-100 sign-in-box m-0">
-					<div className="col-md-8 offset-md-2 bg-white pt-4">
+				<div className="row w-100 m-0">
+					<div className="col-xl-6 pl-0 pr-0 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1 bg-white pt-4 sign-in-box">
 						<p className="display-3 website-logo text-center m-0">
 							Kvora
 						</p>
@@ -14,30 +14,116 @@ class SignIn extends React.Component {
 							A place to share knowledge and better understand the
 							world
 						</p>
-						<div className="d-flex flex-row flex-wrap mb-4 justify-content-around align-items-center">
+						<div className="d-flex flex-row flex-wrap mb-4 pl-3 pr-3 justify-content-around align-items-start">
 							<div className="flex-column justify-content-center align-items-center sign-in-left">
 								<div>
 									<button
 										type="button"
 										className="btn btn-primary"
 									>
-										Primary
+										<i className="fab fa-google mr-3"></i>{" "}
+										Continue with Google
 									</button>
 								</div>
-                                <div>
+								<div>
 									<button
 										type="button"
 										className="btn btn-primary"
 									>
-										Primary
+										<i className="fab fa-facebook-square mr-1"></i>{" "}
+										Continue With Facebook
 									</button>
 								</div>
-                                <div>
-                                </div>
+								<div style={{ width: "238px" }} className="m-0">
+									<p className="text-secondary sign-up-desc">
+										Sign Up With Email. By signing up you
+										indicate that you have read and agree to
+										Quora's Terms of Service and Privacy
+										Policy.
+									</p>
+								</div>
 							</div>
-							<div style={{width:'1px', height:'200px', backgroundColor:'#dadada'}}></div>
-							<div></div>
+							<div
+								className="horizontal-line"
+								style={{
+									display: "none",
+									width: "70%",
+									margin: "20px 0px",
+									height: "1px",
+									backgroundColor: "#dadada"
+								}}
+							></div>
+							<div
+								className="vertical-line"
+								style={{
+									width: "1px",
+									height: "200px",
+									backgroundColor: "#dadada"
+								}}
+							></div>
+							<div className="sign-in-form">
+								<h6>Login</h6>
+								<form>
+									<div className="form-group">
+										<input
+											type="email"
+											className="form-control"
+											aria-describedby="emailHelp"
+											required
+											placeholder="Email"
+										/>
+									</div>
+									<div className="form-group">
+										<input
+											required
+											type="password"
+											className="form-control"
+											placeholder="Password"
+										/>
+									</div>
+									<button
+										type="submit"
+										className="btn btn-primary"
+									>
+										Submit
+									</button>
+								</form>
+							</div>
 						</div>
+						<div>
+							<div
+								className="horizontal-line"
+								style={{
+									width: "100%",
+									margin: "5px 0px",
+									height: "1px",
+									backgroundColor: "#dadada"
+								}}
+							></div>
+							<div className="text-center text-secondary">
+								<small>
+									हिन्दी{" "}
+									<i className="fas fa-chevron-right"></i>
+								</small>
+							</div>
+						</div>
+						<footer className="footer-sign-in-container">
+                            <div className="p-1 w-100 text-secondary w-100 d-flex justify-content-around align-items-center flex-wrap">
+                                <div>About</div>
+							<div>Languages</div>
+							<div>Careers</div>
+							<div>Business</div>
+							<div>Privacy</div>
+							<div>Terms</div>
+							<div>Impressum</div>
+							<div>Contact</div>
+							<div>
+								<i className="far fa-copyright"></i> Kvora Inc.
+								2020
+							</div>
+                            </div>
+							
+						</footer>
 					</div>
 				</div>
 			</div>
