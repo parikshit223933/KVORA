7ec64befe2588ae4b1963cc0ea4e96d933b50b18
Parikshit Singh <pk223933@gmail.com>
2020-08-15 07:11:19 +0530

    Add question in modal UI created

--- client/src/components/HomeFeed.js
+++ client/src/components/HomeFeed.js
@@ -35,7 +35,7 @@ class HomeFeed extends React.Component {
 				</div>
 				{/* MODAL */}
 				<div
-					class="modal fade"
+					className="modal fade"
 					id="exampleModalCenter"
 					tabindex="-1"
 					role="dialog"
@@ -43,37 +43,191 @@ class HomeFeed extends React.Component {
 					aria-hidden="true"
 				>
 					<div
-						class="modal-dialog modal-dialog-centered"
+						className="modal-dialog modal-dialog-centered modal-lg"
 						role="document"
 					>
-						<div class="modal-content">
-							<div class="modal-header">
-								<h5
-									class="modal-title"
+						<div className="modal-content">
+							<div className="modal-header new-question-modal-header pb-0">
+								<b
+									className="modal-title text-secondary"
 									id="exampleModalCenterTitle"
 								>
-									Modal title
-								</h5>
+									<nav>
+										<div
+											className="nav nav-tabs"
+											id="nav-tab"
+											role="tablist"
+										>
+											<a
+												className="nav-link active"
+												style={{
+													backgroundColor:
+														"transparent"
+												}}
+												id="nav-home-tab"
+												data-toggle="tab"
+												href="#nav-home"
+												role="tab"
+												aria-controls="nav-home"
+												aria-selected="true"
+											>
+												Add Question
+											</a>
+											<a
+												className="nav-link"
+												style={{
+													backgroundColor:
+														"transparent"
+												}}
+												id="nav-profile-tab"
+												data-toggle="tab"
+												href="#nav-profile"
+												role="tab"
+												aria-controls="nav-profile"
+												aria-selected="false"
+											>
+												Share Link
+											</a>
+										</div>
+									</nav>
+								</b>
 								<button
 									type="button"
-									class="close"
+									className="close"
 									data-dismiss="modal"
 									aria-label="Close"
 								>
 									<span aria-hidden="true">&times;</span>
 								</button>
 							</div>
-							<div class="modal-body">...</div>
-							<div class="modal-footer">
+							<div className="modal-body px-5">
+								<div
+									className="tab-content"
+									id="nav-tabContent"
+								>
+									<div
+										className="tab-pane fade show active"
+										id="nav-home"
+										role="tabpanel"
+										aria-labelledby="nav-home-tab"
+									>
+										<div className="question-tips mb-3">
+											<ul className="mb-0">
+												<li
+													className="list-unstyled"
+													style={{
+														marginLeft: "-16px"
+													}}
+												>
+													Tips on getting good answers
+													quickly
+												</li>
+												<li className="question-tip">
+													Make sure your question has
+													not been asked already
+												</li>
+												<li className="question-tip">
+													Keep your question short and
+													to the point
+												</li>
+												<li className="question-tip">
+													Double-check grammar and
+													spelling
+												</li>
+											</ul>
+										</div>
+										<div className="d-flex flex-row justify-content-start align-items-center mb-3">
+											<div className="mr-2">
+												<img
+													src={image}
+													alt="user"
+													style={{
+														width: 20,
+														height: 20,
+														borderRadius: "100%"
+													}}
+												/>
+											</div>
+											<div
+												className="text-secondary mr-2"
+												style={{ fontSize: 13 }}
+											>
+												Parikshit Singh Asked
+											</div>
+											<div>
+												<button
+													type="button"
+													style={{
+														borderRadius: 50,
+														border:
+															"1px solid #dadada",
+														paddingBottom: 3,
+														paddingTop: 2,
+														fontSize: 15
+													}}
+													className="btn btn-light"
+												>
+													<i
+														className="fas fa-user-friends"
+														style={{ opacity: 0.5 }}
+													></i>{" "}
+													Public{" "}
+													<i
+														className="fas fa-chevron-down"
+														style={{ opacity: 0.5 }}
+													></i>
+												</button>
+											</div>
+										</div>
+										<div className="mb-3">
+											<input
+												type="text"
+												placeholder='Start your question with "What", "How", "Why", etc.'
+												className="form-control question-input pb-3"
+											/>
+										</div>
+										<div className="d-flex flex-row justify-content-start align-items-center">
+											<div>
+												<i className="fas fa-link"></i>
+											</div>
+											<div className="w-75 ml-1">
+												<input
+													type="text"
+													placeholder="Optional: Include a link that gives context"
+													className="form-control link-provider-input form-control-sm"
+												/>
+											</div>
+										</div>
+                                        <div className="existing-question-suggestions"></div>
+									</div>
+									<div
+										className="tab-pane fade"
+										id="nav-profile"
+										role="tabpanel"
+										aria-labelledby="nav-profile-tab"
+									>
+										adfsd
+									</div>
+								</div>
+							</div>
+							<div
+								className="modal-footer"
+								style={{ backgroundColor: "#f7f7f8" }}
+							>
 								<button
 									type="button"
-									class="btn btn-secondary"
+									className="btn btn-light"
+									style={{ borderRadius: "50px" }}
 									data-dismiss="modal"
 								>
-									Close
+									Cancel
 								</button>
-								<button type="button" class="btn btn-primary">
-									Save changes
+								<button
+									type="button"
+									className="btn btn-primary"
+									style={{ borderRadius: "50px" }}
+								>
+									Add Question
 								</button>
 							</div>
 						</div>
