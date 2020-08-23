44594b2635e1ab33b4a34246d93c8260805eea28
Parikshit Singh <pk223933@gmail.com>
2020-08-23 21:58:09 +0530

    profile header complete

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import '../../assets/css/profile.scss';
+import React from "react";
+import "../../assets/css/profile.scss";
+import image from "../../assets/images/signInBackground.png";
 
-class Profile extends React.Component
-{
-    render()
-    {
-        return<h1 className="pt-5">This is the Profile component.</h1>
-    }
+class Profile extends React.Component {
+	render() {
+		return (
+			<div className="container profile-component bg-white">
+				<div className="row">
+					<div className="col-md-8">
+						<header>
+							<div className="d-flex flex-row justify-content-start align-items-center">
+								<div className="ml-0 my-3 mr-3">
+									<img
+										src={image}
+										alt="user"
+										style={{
+											width: 110,
+											height: 110,
+											borderRadius: "100%"
+										}}
+									/>
+								</div>
+								<div className="d-flex flex-column justify-content-center align-items-start">
+									<h4 className="mb-1"><b>Parikshit Singh</b> <small style={{fontSize:12}} className="text-secondary">Edit</small></h4>
+									<p style={{fontSize:18}} className="m-0">
+										MERN Stack Web Developer | Machine
+										Learning Enthusiast
+									</p>
+                                    <small className="text-secondary">Edit</small>
+								</div>
+							</div>
+                            <div className="text-black-50 font-13">
+                                <div>
+                                    Write a description about yourself
+                                </div>
+                                <div>
+                                    4 Followers
+                                </div>
+                            </div>
+						</header>
+                        <hr/>
+					</div>
+					<div className="col-md-4 d-none d-md-block bg-primary"></div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Profile;
