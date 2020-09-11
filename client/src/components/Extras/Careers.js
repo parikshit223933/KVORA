101949cfcd2a171c24883f5ecd106b36ea588cca
Parikshit Singh <parikshit.singh@codingninjas.com>
2020-09-12 04:20:15 +0530

    careers staged for later

--- client/src/components/Extras/Careers.js
+++ client/src/components/Extras/Careers.js
@@ -5,8 +5,10 @@ class Careers extends React.Component
     render()
     {
         return(
-            <div>
-                This is Careers Component.
+            <div className="container default-padding-top">
+                <h1 className="text-center">
+                    This page is under construction
+                </h1>
             </div>
         )
     }
