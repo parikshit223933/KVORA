82f7a898805b0a0a7ab0941b28035fe8e7b78631
parikshit223933 <parikshit.singh@codingninjas.com>
2020-09-12 22:23:57 +0530

    account settings first part completed

--- client/src/components/Settings/Account.js
+++ client/src/components/Settings/Account.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React from "react";
 
-class Account extends React.Component
-{
-    render()
-    {
-        return(
-            <div>
-                <div>
-                    <b>Account Settings</b>
-                </div>
-                <hr/>
-                <div>
-                    
-                </div>
-            </div>
-        );
-    }
+class Account extends React.Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<div>
+						<b>Account Settings</b>
+					</div>
+					<hr className="my-2" />
+					<div className="py-3 d-flex flex-row align-items-start justify-content-start">
+						<div className="w-25" style={{ minWidth: 150 }}>
+							Primary Email
+						</div>
+						<div>
+							<div>pk223933@gmail.com</div>
+							<div>Add Another Email Address</div>
+						</div>
+					</div>
+					<div className="py-3 d-flex flex-row align-items-start justify-content-start">
+						<div className="w-25" style={{ minWidth: 150 }}>
+							Password
+						</div>
+						<div>
+							<div>Change Password</div>
+						</div>
+					</div>
+					<div className="py-3 d-flex flex-row align-items-start justify-content-start">
+						<div className="w-25" style={{ minWidth: 150 }}>
+							Logout
+						</div>
+						<div>
+							<div>Logout of all other browsers</div>
+						</div>
+					</div>
+					<hr className="my-2" />
+				</div>
+			</div>
+		);
+	}
 }
 export default Account;
