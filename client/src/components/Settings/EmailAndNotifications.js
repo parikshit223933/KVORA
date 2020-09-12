68ce829e9e8056039884bbfb705ffcfc9183d04a
parikshit223933 <pk223933@gmail.com>
2020-09-13 02:48:47 +0530

    email and notification essction designed

--- client/src/components/Settings/EmailAndNotifications.js
+++ client/src/components/Settings/EmailAndNotifications.js
@@ -87,11 +87,7 @@ class EmailAndNotifications extends React.Component {
 					</div>
 				</div>
 
-
-
-
-
-                <div
+				<div
 					style={{
 						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
 						borderRadius: 5,
@@ -170,9 +166,348 @@ class EmailAndNotifications extends React.Component {
 					</div>
 				</div>
 
+				<div
+					style={{
+						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
+						borderRadius: 5,
+					}}
+					className="mb-3"
+				>
+					<div className="p-3 font-heavy">
+						General Questions & Answers
+					</div>
+					<hr className="m-0" />
+					<div>
+						<div className="p-3">
+							<div className="form-group form-check mb-0">
+								<input
+									type="checkbox"
+									className="form-check-input"
+									id="exampleCheck1"
+								/>
+								<label
+									className="form-check-label"
+									for="exampleCheck1"
+								>
+									Check me out
+								</label>
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+					<div>
+						<div className="p-3">
+							<div className="form-group form-check mb-0">
+								<input
+									type="checkbox"
+									className="form-check-input"
+									id="exampleCheck1"
+								/>
+								<label
+									className="form-check-label"
+									for="exampleCheck1"
+								>
+									Check me out
+								</label>
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+					<div>
+						<div className="p-3 d-flex flex-row justify-content-between align-items-center">
+							<div className="form-group mb-0">
+								Check me out
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+							<div>
+								<button
+									type="button"
+									className="btn blue-tint curved-button"
+								>
+									Manage
+								</button>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+				</div>
 
+				<div
+					style={{
+						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
+						borderRadius: 5,
+					}}
+					className="mb-3"
+				>
+					<div className="p-3 font-heavy">
+						General Questions & Answers
+					</div>
+					<hr className="m-0" />
+					<div>
+						<div className="p-3">
+							<div className="form-group form-check mb-0">
+								<input
+									type="checkbox"
+									className="form-check-input"
+									id="exampleCheck1"
+								/>
+								<label
+									className="form-check-label"
+									for="exampleCheck1"
+								>
+									Check me out
+								</label>
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+					<div>
+						<div className="p-3 d-flex flex-row justify-content-between align-items-center">
+							<div className="form-group mb-0">
+								Check me out
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+							<div>
+								<button
+									type="button"
+									className="btn blue-tint curved-button"
+								>
+									Manage
+								</button>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+					<div>
+						<div className="p-3 d-flex flex-row justify-content-between align-items-center">
+							<div className="form-group mb-0">
+								Check me out
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+							<div>
+								<button
+									type="button"
+									className="btn blue-tint curved-button"
+								>
+									Manage
+								</button>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+				</div>
 
-                <div
+				<div className="py-2">
+					<b>Activity on Your Content</b>
+				</div>
+				<hr className="mt-2 mb-4" />
+
+				<div
+					style={{
+						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
+						borderRadius: 5,
+					}}
+					className="mb-3"
+				>
+					<div className="p-3 font-heavy">
+						General Questions & Answers
+					</div>
+					<hr className="m-0" />
+					<div>
+						<div className="p-3">
+							<div className="form-group form-check mb-0">
+								<input
+									type="checkbox"
+									className="form-check-input"
+									id="exampleCheck1"
+								/>
+								<label
+									className="form-check-label"
+									for="exampleCheck1"
+								>
+									Check me out
+								</label>
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+				</div>
+
+				<div
+					style={{
+						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
+						borderRadius: 5,
+					}}
+					className="mb-3"
+				>
+					<div className="p-3 font-heavy">
+						General Questions & Answers
+					</div>
+					<hr className="m-0" />
+					<div>
+						<div className="p-3">
+							<div className="form-group form-check mb-0">
+								<input
+									type="checkbox"
+									className="form-check-input"
+									id="exampleCheck1"
+								/>
+								<label
+									className="form-check-label"
+									for="exampleCheck1"
+								>
+									Check me out
+								</label>
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+				</div>
+
+				<div
+					style={{
+						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
+						borderRadius: 5,
+					}}
+					className="mb-3"
+				>
+					<div className="p-3 font-heavy">
+						General Questions & Answers
+					</div>
+					<hr className="m-0" />
+					<div>
+						<div className="p-3">
+							<div className="form-group form-check mb-0">
+								<input
+									type="checkbox"
+									className="form-check-input"
+									id="exampleCheck1"
+								/>
+								<label
+									className="form-check-label"
+									for="exampleCheck1"
+								>
+									Check me out
+								</label>
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+						</div>
+						<hr className="my-0" />
+					</div>
+				</div>
+
+				<div className="py-2">
+					<b>From Quora</b>
+				</div>
+				<hr className="mt-2 mb-4" />
+
+				<div
+					style={{
+						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
+						borderRadius: 5,
+					}}
+					className="mb-3"
+				>
+					<div className="p-3 font-heavy">Quora Digest</div>
+					<hr className="m-0" />
+					<div className="pb-4">
+						<div className="p-3">
+							<div className="form-group form-check mb-0">
+								<input
+									type="checkbox"
+									className="form-check-input"
+									id="exampleCheck1"
+								/>
+								<label
+									className="form-check-label"
+									for="exampleCheck1"
+								>
+									Check me out
+								</label>
+								<div className="text-secondary">
+									Email me when there are new answers to
+									questions I asked or follow.
+								</div>
+							</div>
+						</div>
+						<div className="d-flex flex-row justify-content-center align-items-center">
+							<div className="mx-2">Email Frequency:</div>
+							<div className="form-check mx-2">
+								<input
+									className="form-check-input"
+									type="radio"
+									name="exampleRadios"
+									id="exampleRadios1"
+									value="option1"
+									checked
+								/>
+								<label
+									className="form-check-label"
+									for="exampleRadios1"
+								>
+									<b>Default radio</b>
+								</label>
+								<div className="text-secondary font-13">
+									This setting is shared with push
+									notifications
+								</div>
+							</div>
+							<div className="form-check mx-2">
+								<input
+									className="form-check-input"
+									type="radio"
+									name="exampleRadios"
+									id="exampleRadios2"
+									value="option2"
+								/>
+								<label
+									className="form-check-label"
+									for="exampleRadios2"
+								>
+									<b>Second default radio</b>
+								</label>
+								<div className="text-secondary font-13">
+									This setting is shared with push
+									notifications
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div
 					style={{
 						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
 						borderRadius: 5,
@@ -250,92 +585,6 @@ class EmailAndNotifications extends React.Component {
 						<hr className="my-0" />
 					</div>
 				</div>
-
-
-
-                <div
-					style={{
-						boxShadow: "0px 0px 1px 1px rgb(216, 216, 216)",
-						borderRadius: 5,
-					}}
-					className="mb-3"
-				>
-					<div className="p-3 font-heavy">
-						General Questions & Answers
-					</div>
-					<hr className="m-0" />
-					<div>
-						<div className="p-3">
-							<div className="form-group form-check mb-0">
-								<input
-									type="checkbox"
-									className="form-check-input"
-									id="exampleCheck1"
-								/>
-								<label
-									className="form-check-label"
-									for="exampleCheck1"
-								>
-									Check me out
-								</label>
-								<div className="text-secondary">
-									Email me when there are new answers to
-									questions I asked or follow.
-								</div>
-							</div>
-						</div>
-						<hr className="my-0" />
-					</div>
-					<div>
-						<div className="p-3">
-							<div className="form-group form-check mb-0">
-								<input
-									type="checkbox"
-									className="form-check-input"
-									id="exampleCheck1"
-								/>
-								<label
-									className="form-check-label"
-									for="exampleCheck1"
-								>
-									Check me out
-								</label>
-								<div className="text-secondary">
-									Email me when there are new answers to
-									questions I asked or follow.
-								</div>
-							</div>
-						</div>
-						<hr className="my-0" />
-					</div>
-					<div>
-						<div className="p-3">
-							<div className="form-group form-check mb-0">
-								<input
-									type="checkbox"
-									className="form-check-input"
-									id="exampleCheck1"
-								/>
-								<label
-									className="form-check-label"
-									for="exampleCheck1"
-								>
-									Check me out
-								</label>
-								<div className="text-secondary">
-									Email me when there are new answers to
-									questions I asked or follow.
-								</div>
-							</div>
-						</div>
-						<hr className="my-0" />
-					</div>
-				</div>
-
-
-
-
-                
 			</div>
 		);
 	}
