4709a0e04cef7d4181130cf43dda2886b9399456
parikshit223933 <pk223933@gmail.com>
2020-09-21 21:08:40 +0530

    updated notificatiob schema, created post schema (minimalistic)

---
+++ server/models/post.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+	{
+		type: {
+			type: String,
+		},
+		question: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Question',
+		},
+		answer: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Answer',
+		},
+		author: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
+		thanks: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
+		upvotes: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
+		shares: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Share',
+			},
+		],
+		comments: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Comment',
+			},
+		],
+		associatedSpaces: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Space',
+			},
+		],
+	},
+	{
+		timestamps: true,
+		validateBeforeSave: true,
+	}
+);
--- server/models/user.js
+++ server/models/user.js
@@ -1,124 +1,136 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	firstName: {
-		type: String,
-		required: true,
-	},
-	lastName: {
-		type: String,
-		required: true,
-	},
-	avatar: {
-		type: String,
-	},
-	title: {
-		type: String,
-	},
-	description: {
-		type: String,
-	},
-	google: {
-		type: String,
-	},
-	twitter: {
-		type: String,
-	},
-	facebook: {
-		type: String,
-	},
-	spaces: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Space',
-			muted: Boolean,
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
 		},
-	],
-	knowsAbout: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Topic',
-			muted: Boolean,
-			bookmarked: Boolean,
+		password: {
+			type: String,
+			required: true,
 		},
-	],
-	thankedTopicLogs: [
-		{
-			topicLog: {
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		avatar: {
+			type: String,
+		},
+		title: {
+			type: String,
+		},
+		description: {
+			type: String,
+		},
+		google: {
+			type: String,
+		},
+		twitter: {
+			type: String,
+		},
+		facebook: {
+			type: String,
+		},
+		notifications: [
+			{
 				type: mongoose.Schema.Types.ObjectId,
-				ref: 'TopicLog',
+				ref: 'Notification',
 			},
-			isThanked: Boolean,
-		},
-	],
-	credentials: [
-		{
-			name: String,
-			isDefault: Boolean,
-		},
-	],
-	questions: [
-		{
+		],
+		spaces: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Space',
+				muted: Boolean,
+			},
+		],
+		knowsAbout: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Topic',
+				muted: Boolean,
+				bookmarked: Boolean,
+			},
+		],
+		thankedTopicLogs: [
+			{
+				topicLog: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'TopicLog',
+				},
+				isThanked: Boolean,
+			},
+		],
+		credentials: [
+			{
+				name: String,
+				isDefault: Boolean,
+			},
+		],
+		questions: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Question',
+			},
+		],
+		answers: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Answer',
+			},
+		],
+		shares: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Share',
+			},
+		],
+		posts: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Post',
+			},
+		],
+		followers: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
+		following: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
+		edits: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Edit',
+			},
+		],
+		activity: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Activity',
+			},
+		],
+		settings: {
 			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Question',
+			ref: 'Setting',
 		},
-	],
-	answers: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Answer',
-		},
-	],
-	shares: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Share',
-		},
-	],
-	posts: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Post',
-		},
-	],
-	followers: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-		},
-	],
-	following: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-		},
-	],
-	edits: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Edit',
-		},
-	],
-	activity: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Activity',
-		},
-	],
-	settings: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Setting',
 	},
-});
+	{
+		timestamps: true,
+		validateBeforeSave: true,
+	}
+);
 
 const user = mongoose.model('User', userSchema);
 module.exports = user;
